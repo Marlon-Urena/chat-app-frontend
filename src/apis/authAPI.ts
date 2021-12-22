4b19c177784e84f9b88555b765ba77c944b900f4
Marlon <marlon.urena@cix.csi.cuny.edu>
2021-12-21 20:22:26 -0500

    Added new endpoint to access userService API

--- src/apis/authAPI.ts
+++ src/apis/authAPI.ts
@@ -2,19 +2,20 @@ import axios, { AxiosResponse } from 'axios';
 import firebase from 'firebase/compat';
 import { User } from '../store/authentication/types';
 
-const authenticationURL = 'http://localhost:8443';
+const authenticationServiceURL = 'http://localhost:3002/auth';
+const userServiceURL = 'http://localhost:8443';
 
 async function createUser(signupDetails: {
   username: string;
   email: string;
   password: string;
 }): Promise<AxiosResponse> {
-  return axios.post(`${authenticationURL}/register`, signupDetails);
+  return axios.post(`${authenticationServiceURL}/signup`, signupDetails);
 }
 
 async function getUser(): Promise<AxiosResponse<User>> {
   const user = firebase.auth().currentUser;
-  return axios.post(`${authenticationURL}/user`, undefined, {
+  return axios.post(`${userServiceURL}/user`, undefined, {
     headers: { Authorization: `Bearer ${await user?.getIdToken()}` }
   });
 }
