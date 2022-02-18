8ac3d9c4ce5b2bcb851473f60d0ac2b54ad87d6c
Marlon-Urena <anewryurena@gmail.com>
2022-02-18 15:31:41 +0000

    fixed bug whereupon pressing register button and failing, the button will continue to have a loading indicator.

--- src/components/authentication/register/RegisterForm.tsx
+++ src/components/authentication/register/RegisterForm.tsx
@@ -41,8 +41,9 @@ export default function RegisterForm() {
       password: ''
     },
     validationSchema: RegisterSchema,
-    onSubmit: (values) => {
+    onSubmit: (values, actions) => {
       dispatch(register(values));
+      actions.setSubmitting(false);
     }
   });
 
